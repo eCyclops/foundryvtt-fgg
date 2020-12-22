0573cd60a6f08353ada36e6145c692e9aef2bc26
Barry Dewey-Robertson <gmsshadow2@gmail.com>
2020-12-22 23:50:38 +0000

    Update settings.js

--- src/module/settings.js
+++ src/module/settings.js
@@ -1,36 +1,36 @@
 export const registerSettings = function () {
 
   game.settings.register("fgg", "initiative", {
-    name: game.i18n.localize("OSE.Setting.Initiative"),
-    hint: game.i18n.localize("OSE.Setting.InitiativeHint"),
+    name: game.i18n.localize("FGG.Setting.Initiative"),
+    hint: game.i18n.localize("FGG.Setting.InitiativeHint"),
     default: "group",
     scope: "world",
     type: String,
     config: true,
     choices: {
-      individual: "OSE.Setting.InitiativeIndividual",
-      group: "OSE.Setting.InitiativeGroup",
+      individual: "FGG.Setting.InitiativeIndividual",
+      group: "FGG.Setting.InitiativeGroup",
     },
     onChange: _ => window.location.reload()
   });
 
   game.settings.register("fgg", "rerollInitiative", {
-    name: game.i18n.localize("OSE.Setting.RerollInitiative"),
-    hint: game.i18n.localize("OSE.Setting.RerollInitiativeHint"),
+    name: game.i18n.localize("FGG.Setting.RerollInitiative"),
+    hint: game.i18n.localize("FGG.Setting.RerollInitiativeHint"),
     default: "reset",
     scope: "world",
     type: String,
     config: true,
     choices: {
-      keep: "OSE.Setting.InitiativeKeep",
-      reset: "OSE.Setting.InitiativeReset",
-      reroll: "OSE.Setting.InitiativeReroll",
+      keep: "FGG.Setting.InitiativeKeep",
+      reset: "FGG.Setting.InitiativeReset",
+      reroll: "FGG.Setting.InitiativeReroll",
     }
   });
 
   game.settings.register("fgg", "ascendingAC", {
-    name: game.i18n.localize("OSE.Setting.AscendingAC"),
-    hint: game.i18n.localize("OSE.Setting.AscendingACHint"),
+    name: game.i18n.localize("FGG.Setting.AscendingAC"),
+    hint: game.i18n.localize("FGG.Setting.AscendingACHint"),
     default: false,
     scope: "world",
     type: Boolean,
@@ -39,8 +39,8 @@ export const registerSettings = function () {
   });
 
   game.settings.register("fgg", "morale", {
-    name: game.i18n.localize("OSE.Setting.Morale"),
-    hint: game.i18n.localize("OSE.Setting.MoraleHint"),
+    name: game.i18n.localize("FGG.Setting.Morale"),
+    hint: game.i18n.localize("FGG.Setting.MoraleHint"),
     default: false,
     scope: "world",
     type: Boolean,
@@ -48,24 +48,24 @@ export const registerSettings = function () {
   });
 
   game.settings.register("fgg", "encumbranceOption", {
-    name: game.i18n.localize("OSE.Setting.Encumbrance"),
-    hint: game.i18n.localize("OSE.Setting.EncumbranceHint"),
+    name: game.i18n.localize("FGG.Setting.Encumbrance"),
+    hint: game.i18n.localize("FGG.Setting.EncumbranceHint"),
     default: "detailed",
     scope: "world",
     type: String,
     config: true,
     choices: {
-      disabled: "OSE.Setting.EncumbranceDisabled",
-      basic: "OSE.Setting.EncumbranceBasic",
-      detailed: "OSE.Setting.EncumbranceDetailed",
-      complete: "OSE.Setting.EncumbranceComplete",
+      disabled: "FGG.Setting.EncumbranceDisabled",
+      basic: "FGG.Setting.EncumbranceBasic",
+      detailed: "FGG.Setting.EncumbranceDetailed",
+      complete: "FGG.Setting.EncumbranceComplete",
     },
     onChange: _ => window.location.reload()
   });
 
   game.settings.register("fgg", "significantTreasure", {
-    name: game.i18n.localize("OSE.Setting.SignificantTreasure"),
-    hint: game.i18n.localize("OSE.Setting.SignificantTreasureHint"),
+    name: game.i18n.localize("FGG.Setting.SignificantTreasure"),
+    hint: game.i18n.localize("FGG.Setting.SignificantTreasureHint"),
     default: 800,
     scope: "world",
     type: Number,
