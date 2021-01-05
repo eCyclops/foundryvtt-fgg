38f3429d644ab9ea34ca6556dc7755f62064cdee
Barry Dewey-Robertson <gmsshadow2@gmail.com>
2021-01-05 17:27:12 +0000

    Update dice.js

--- src/module/dice.js
+++ src/module/dice.js
@@ -32,7 +32,7 @@ export class FggDice {
     } else if (data.roll.type == "table") {
       // Reaction
       let table = data.roll.table;
-      let output = "";
+      let output = Object.values(table)[0];
       for (let i = 0; i <= roll.total; i++) {
         if (table[i]) {
           output = table[i];
@@ -305,7 +305,7 @@ export class FggDice {
         callback: (html) => {
           rolled = true;
           rollData.form = html[0].querySelector("form");
-          rollData.data.roll.target = parseInt(rollData.data.roll.target) + parseInt(rollData.data.roll.magic);
+          rollData.parts.push(`${rollData.data.roll.magic}`);
           rollData.title += ` ${game.i18n.localize("FGG.saves.magic.short")} (${rollData.data.roll.magic})`;
           roll = FggDice.sendRoll(rollData);
         },
