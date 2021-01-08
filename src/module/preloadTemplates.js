167fe4d1e8c0568d23cb979459cb5df7cfc4b292
Barry Dewey-Robertson <gmsshadow2@gmail.com>
2021-01-08 16:02:07 +0000

    Update preloadTemplates.js

--- src/module/preloadTemplates.js
+++ src/module/preloadTemplates.js
@@ -8,6 +8,7 @@ export const preloadHandlebarsTemplates = async function () {
         'systems/fgg/templates/actors/partials/character-header.html',
         'systems/fgg/templates/actors/partials/character-attributes-tab.html',
         'systems/fgg/templates/actors/partials/character-abilities-tab.html',
+        'systems/fgg/templates/actors/partials/character-noncombat-tab.html',
         'systems/fgg/templates/actors/partials/character-spells-tab.html',
         'systems/fgg/templates/actors/partials/character-inventory-tab.html',
         'systems/fgg/templates/actors/partials/character-notes-tab.html',
